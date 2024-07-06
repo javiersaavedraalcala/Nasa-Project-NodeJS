0c944ba95f3370e6c0c109ffd6e8b968f35a95b0
Javier Saavedra <javiersaavedra@Javiers-Mac-mini.local>
2024-07-06 13:24:52 -0600

    Updates package master

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,8 @@
-const http = require('http');
+const http = require("http");
 
-const app = require('./app');
+const app = require("./app");
 
-const { loadPlanetsData } = require('./models/planets.model');
+const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 async function startServer() {
   await loadPlanetsData();
-  
+
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
